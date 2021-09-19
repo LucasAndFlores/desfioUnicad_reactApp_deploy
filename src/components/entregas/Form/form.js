8229e23d5ae4a40685bc94d7fa64ffff636e5c56
lucasandflores <lcs.oliveira93@gmail.com>
2021-09-19 18:28:02 -0300

    form + logo

--- src/components/entregas/Form/form.js
+++ src/components/entregas/Form/form.js
@@ -1,31 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import axios from 'axios'
+import './form.css'
 
-const formEntrega = () => {
+const initialValue = {
+    nome_cliente: "",
+    data_de_entrega: "",
+    ponto_de_partida: "",
+    num_ponto_de_partida: 0,
+    ponto_de_destino: "",
+    num_ponto_de_destino: 0
+}
+
+const FormEntrega = () => {
+
+  
+    const [values, setValues] = useState(initialValue);
+ 
+
+    function onChange (env) {
+        const { name, value } = env.target
+
+        setValues({...values, [name]: value})
+
+
+    }
+
+    function onSubmit (env) {
+        env.preventDefault()
+
+        axios.post('http://localhost:8000/v1/entregas/', values)
+
+
+    }
+
 
 
 
     return (
     
 
-    <Container>
+    <Container className="styledContainer">
 
         <h1> Preencha suas entregas </h1>
 
-        <Form>
+        <Form onSubmit={onSubmit}>
             <Row>
                 <Col>
                     <Form.Group className="mb-3" controlId="NomeCliente">
                     <Form.Label>Nome do cliente</Form.Label>
-                    <Form.Control type="text" placeholder="Nome do cliente" />
+                    <Form.Control type="text" placeholder="Nome do cliente" 
+                    onChange={onChange} name="nome_cliente" />
                     </Form.Group>
                 </Col>
 
                 <Col lg="4" md="4">
                     <Form.Group className="mb-3" controlId="DataDeEntrega">
                     <Form.Label>Data de Entrega</Form.Label>
-                    <Form.Control type="date" placeholder="Insira a data de entrega" />
+                    <Form.Control type="date" placeholder="Insira a data de entrega" 
+                    onChange={onChange} name="data_de_entrega" />
                     </Form.Group>
                 </Col>
             </Row>
@@ -33,14 +68,16 @@ const formEntrega = () => {
             <Row>
                 <Col>
                     <Form.Group className="mb-3" controlId="PontoDePartida">
-                    <Form.Label>Insira o ponto de partida</Form.Label>
-                    <Form.Control type="text" placeholder="Insira o ponto de partida" />
+                    <Form.Label> Ponto de partida</Form.Label>
+                    <Form.Control type="text" placeholder="Ponto de partida" 
+                    onChange={onChange} name="ponto_de_partida"/>
                     </Form.Group>
                 </Col>
                 <Col lg="4" md="4">
                     <Form.Group className="mb-3" controlId="nPontoDePartida">
-                    <Form.Label>Insira numero do ponto de partida</Form.Label>
-                    <Form.Control type="text" placeholder="Insira numero do ponto de partida" />
+                    <Form.Label> Numero do ponto de partida</Form.Label>
+                    <Form.Control type="text" placeholder=" Numero do ponto de partida" 
+                    onChange={onChange} name="num_ponto_de_partida" />
                     </Form.Group>
                 </Col>
 
@@ -49,23 +86,26 @@ const formEntrega = () => {
             <Row>
                 <Col>
                     <Form.Group className="mb-3" controlId="PontoDeDestino">
-                    <Form.Label>Insira o ponto de destino</Form.Label>
-                    <Form.Control type="text" placeholder="Insira o ponto de destino" />
+                    <Form.Label> Ponto de destino</Form.Label>
+                    <Form.Control type="text" placeholder=" Ponto de destino" 
+                    onChange={onChange} name="ponto_de_destino" />
                     </Form.Group>
                 </Col>
 
                 <Col lg="4" md="4">
                     <Form.Group className="mb-3" controlId="nPontoDeDestino">
-                    <Form.Label>Insira numero do ponto de destino</Form.Label>
-                    <Form.Control type="text" placeholder="Insira numero do ponto de destino" />
+                    <Form.Label> Numero do ponto de destino</Form.Label>
+                    <Form.Control type="text" placeholder="Numero do ponto de destino" 
+                    onChange={onChange} name="num_ponto_de_destino" />
                     </Form.Group>
                 </Col>
             </Row>
-            
+            < Button type="submit" variant="outline-primary">Enviar</Button>
         </Form>
+
     </Container>
     )
 
 }
 
-export default formEntrega
+export default FormEntrega
